4c43c817813974e7f5e9e06ba8937bf73194b44e
aattoa <aattoa@proton.me>
2024-06-12 12:52:32 +0300

    json: Basic encoding

--- src/json/cpputil/json.hpp
+++ src/json/cpputil/json.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <expected>
+#include <format>
+#include <iterator>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -18,18 +20,18 @@ namespace cpputil::inline v0::json {
         typename T::Boolean;
     };
 
-    struct Default_configuration;
+    template <configuration Config>
+    using Value_variant = std::variant<
+        std::monostate,
+        typename Config::Object,
+        typename Config::Array,
+        typename Config::String,
+        typename Config::Number,
+        typename Config::Boolean>;
 
-    template <class Config = Default_configuration>
-    struct Value {
-        using Variant = std::variant<
-            std::monostate,
-            typename Config::Object,
-            typename Config::Array,
-            typename Config::String,
-            typename Config::Number,
-            typename Config::Boolean>;
-        Variant variant;
+    template <class Config> // Unconstrained to enable recursive types
+    struct Basic_value {
+        Value_variant<Config> variant;
 
         [[nodiscard]] constexpr auto is_null() const noexcept -> bool
         {
@@ -93,8 +95,8 @@ namespace cpputil::inline v0::json {
     };
 
     struct Default_configuration {
-        using Object  = std::unordered_map<std::string, Value<Default_configuration>>;
-        using Array   = std::vector<Value<Default_configuration>>;
+        using Object  = std::unordered_map<std::string, Basic_value<Default_configuration>>;
+        using Array   = std::vector<Basic_value<Default_configuration>>;
         using String  = std::string;
         using Number  = double;
         using Boolean = bool;
@@ -102,12 +104,81 @@ namespace cpputil::inline v0::json {
         static_assert(configuration<Default_configuration>);
     };
 
+    using Value = Basic_value<Default_configuration>;
+
+    template <std::output_iterator<char> Out, class Arithmetic>
+        requires std::is_arithmetic_v<Arithmetic>
+    auto to_json(Out out, Arithmetic const arithmetic) -> Out
+    {
+        return std::format_to(out, "{}", arithmetic);
+    }
+
+    template <std::output_iterator<char> Out>
+    auto to_json(Out out, std::string_view const string) -> Out
+    {
+        return std::format_to(out, "\"{}\"", string);
+    }
+
+    template <std::output_iterator<char> Out>
+    auto to_json(Out out, std::monostate) -> Out
+    {
+        return std::format_to(out, "null");
+    }
+
+    template <std::output_iterator<char> Out, std::input_iterator It, std::sentinel_for<It> End>
+    auto range_to_json(Out out, char const open, char const close, It it, End const end) -> Out
+    {
+        *out++ = open;
+        if (it != end) {
+            out = to_json(out, *it);
+            while (++it != end) {
+                *out++ = ',';
+                out    = to_json(out, *it);
+            }
+        }
+        *out++ = close;
+        return out;
+    }
+
+    template <std::output_iterator<char> Out, class A, class B>
+    auto to_json(Out out, std::pair<A, B> const& pair) -> Out
+    {
+        out    = to_json(out, pair.first);
+        *out++ = ':';
+        out    = to_json(out, pair.second);
+        return out;
+    }
+
+    template <std::output_iterator<char> Out, class... Ts>
+    auto to_json(Out out, std::vector<Ts...> const& vector) -> Out
+    {
+        return range_to_json(out, '[', ']', vector.begin(), vector.end());
+    }
+
+    template <std::output_iterator<char> Out, class... Ts>
+    auto to_json(Out out, std::unordered_map<Ts...> const& map) -> Out
+    {
+        return range_to_json(out, '{', '}', map.begin(), map.end());
+    }
+
+    template <std::output_iterator<char> Out, configuration Config>
+    auto to_json(Out out, Basic_value<Config> const& value) -> Out
+    {
+        return std::visit(
+            [&](auto const& alternative) { return to_json(out, alternative); }, value.variant);
+    }
+
+    template <configuration Config = Default_configuration>
+    [[nodiscard]] auto encode(Basic_value<Config> const& value) -> std::string
+    {
+        std::string output;
+        to_json(std::back_inserter(output), value);
+        return output;
+    }
+
     struct Parse_error {};
 
-    template <configuration Configuration = Default_configuration>
-    [[nodiscard]] auto decode(std::string_view) -> std::expected<Value<Configuration>, Parse_error>;
-
-    template <configuration Configuration = Default_configuration>
-    [[nodiscard]] auto encode(Value<Configuration> const&) -> std::string;
+    template <configuration Config = Default_configuration>
+    [[nodiscard]] auto decode(std::string_view) -> std::expected<Basic_value<Config>, Parse_error>;
 
 } // namespace cpputil::inline v0::json
