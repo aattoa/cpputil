29088ac91a6baa331bbdfa4135f3f05917b7d740
aattoa <aattoa@proton.me>
2025-03-23 14:25:53 +0200

    Add library cpputil::num

---
+++ src/num/cpputil/num.cpp
@@ -0,0 +1,14 @@
+#include <cpputil/num.hpp>
+
+cpputil::num::Safe_cast_invalid_argument::Safe_cast_invalid_argument()
+    : invalid_argument("cpputil::num::safe_cast argument out of target range")
+{}
+
+static_assert(cpputil::num::losslessly_convertible<std::int8_t, std::int16_t>);
+static_assert(cpputil::num::losslessly_convertible<std::int32_t, std::int32_t>);
+static_assert(cpputil::num::losslessly_convertible<std::uint8_t, std::int32_t>);
+static_assert(cpputil::num::losslessly_convertible<std::uint32_t, std::int64_t>);
+static_assert(not cpputil::num::losslessly_convertible<std::int8_t, std::uint8_t>);
+static_assert(not cpputil::num::losslessly_convertible<std::uint64_t, std::int8_t>);
+static_assert(not cpputil::num::losslessly_convertible<std::int8_t, std::uint64_t>);
+static_assert(not cpputil::num::losslessly_convertible<std::int16_t, std::int8_t>);
---
+++ src/num/cpputil/num.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <concepts>
+#include <stdexcept>
+#include <utility>
+
+namespace cpputil::inline v0::num {
+
+    template <typename From, typename To>
+    concept losslessly_convertible = std::integral<From> and std::integral<To>
+                                 and std::in_range<To>(std::numeric_limits<From>::min())
+                                 and std::in_range<To>(std::numeric_limits<From>::max());
+
+    struct Safe_cast_invalid_argument : std::invalid_argument {
+        Safe_cast_invalid_argument();
+    };
+
+    template <std::integral To, std::integral From>
+    [[nodiscard]] constexpr auto safe_cast(From const from)
+        noexcept(losslessly_convertible<From, To>) -> To
+    {
+        if constexpr (not losslessly_convertible<From, To>) {
+            if (not std::in_range<To>(from)) {
+                throw Safe_cast_invalid_argument {};
+            }
+        }
+        return static_cast<To>(from);
+    }
+
+} // namespace cpputil::inline v0::num
